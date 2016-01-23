6b1ee15a867b521991338393c4dd0390aa0a14b3
jjseymour <jjseymour@gmail.com>
2016-01-23 00:55:51 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number != 0
+    number -= 1
+    puts "#{number} SECOND(S)"
+    sleep(1)
+  end
 end
--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -1,7 +1,8 @@
 require_relative './spec_helper'
+require_relative '../countdown.rb'
 require 'benchmark'
 
-describe 'LOOPS' do
+describe "LOOPS" do
 
   describe '#countdown' do
     let(:countdown_output) { "10 SECOND(S)!\n9 SECOND(S)!\n8 SECOND(S)!\n7 SECOND(S)!\n6 SECOND(S)!\n5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\n" }
